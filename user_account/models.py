23231cbe84bd4e57dea59457b6e9f26d615fa30c
pedramjlo <pedram_sif@tuta.io>
2024-12-02 19:04:15 +0330

    modified create superuser

--- user_account/models.py
+++ user_account/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractUser
-from phonenumber_field.formfields import PhoneNumberField
+
 
 
 class CustomUserManager(BaseUserManager):
@@ -19,24 +19,28 @@ class CustomUserManager(BaseUserManager):
     we are basically overriding Django's built-in createsuperuser terminal command
     and adding extra fields to create a superuser there
     """
-    def create_user(self, email, password=None, **extra_fields): 
-        if not email: 
-            raise ValueError('The Email field must be set') 
-        email = self.normalize_email(email) 
-        user = self.model(email=email, **extra_fields) 
-        user.set_password(password) 
-        user.save(using=self._db) 
-        return user
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True) 
+        extra_fields.setdefault('is_superuser', True) 
+        if extra_fields.get('is_staff') is not True: 
+            raise ValueError('Superuser must have is_staff=True.') 
+        if extra_fields.get('is_superuser') is not True: 
+            raise ValueError('Superuser must have is_superuser=True.') 
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    email = models.EmailField(unique=True)
-    phone_number = PhoneNumberField(max_length=11, region="IR")
-
-    objects = CustomUserManager()
-
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+    email = models.EmailField(('email address'), unique=True) 
+    first_name = models.CharField(max_length=50) 
+    last_name = models.CharField(max_length=50) 
+    phone_number = models.CharField(max_length=11, blank=True) 
+    is_active = models.BooleanField(default=True) 
+    is_staff = models.BooleanField(default=False) 
+    
+    objects = CustomUserManager() 
+    
+    USERNAME_FIELD = 'email' 
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number'] 
+    
+    def __str__(self): 
+        return self.email
