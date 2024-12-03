4fb6f6158e28fd800386a833560747e4ecda34bf
pedramjlo <pedram_sif@tuta.io>
2024-12-03 15:31:23 +0330

    merged models

--- user_account/models.py
+++ user_account/models.py
@@ -1,6 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractUser
-
+from django.contrib.auth.models import BaseUserManager, AbstractUser, PermissionsMixin
 
 
 class CustomUserManager(BaseUserManager):
@@ -13,12 +13,6 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-
-
-    """
-    we are basically overriding Django's built-in createsuperuser terminal command
-    and adding extra fields to create a superuser there
-    """
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True) 
         extra_fields.setdefault('is_superuser', True) 
@@ -29,18 +23,34 @@ class CustomUserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-class CustomUser(AbstractUser):
+class CustomUser(AbstractUser, PermissionsMixin):
+
+    class NationalityChoice(models.TextChoices):
+        IRANIAN = 'iranian', 'Iranian'
+        FOREIGNER = 'foreigner', 'Foreigner'
+
     email = models.EmailField(('email address'), unique=True) 
     first_name = models.CharField(max_length=50) 
     last_name = models.CharField(max_length=50) 
     phone_number = models.CharField(max_length=11, blank=True) 
     is_active = models.BooleanField(default=True) 
-    is_staff = models.BooleanField(default=False) 
-    
+    is_staff = models.BooleanField(default=False)
+    nationality = models.CharField(max_length=9, choices=NationalityChoice.choices, null=False, blank=False)
+    national_id_number = models.PositiveBigIntegerField(null=True, blank=True)
+    passport_number = models.CharField(max_length=9, null=True, blank=True)
+
     objects = CustomUserManager() 
     
     USERNAME_FIELD = 'email' 
-    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number'] 
-    
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'phone_number', 'nationality', 'national_id_number', 'passport_number'] 
+
+
+    def clean(self):
+        super().clean()
+        if self.nationality == self.NationalityChoice.IRANIAN and not self.national_id_number:
+            raise ValidationError("Iranian nationals must enter a national ID number")
+        if self.nationality == self.NationalityChoice.FOREIGNER and not self.passport_number:
+            raise ValidationError("Non-Iranian nationals must enter a passport number")
+
     def __str__(self): 
         return self.email
