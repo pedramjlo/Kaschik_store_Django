1ea337eb0f96f176bc9d8d26a0b430ffef463907
pedramjlo <pedram_sif@tuta.io>
2024-12-09 00:44:32 +0330

    modified shoppingcart mechanism

--- shopping_cart/utility.py
+++ shopping_cart/utility.py
@@ -1,23 +1,33 @@
 from django.shortcuts import get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
 
 from .models import ShoppingCart, ShoppingCartItem
 from products.models import Product
-
 
 class ShoppingCartMechanism:
     def __init__(self, user, status='active'):
         self.user = user
         self.cart, created = ShoppingCart.objects.get_or_create(user=user, status=status)
 
-
     def get_cart(self):
         return self.cart
 
+    def update_cart_status(self):
+        if ShoppingCart.objects.count() >= 1:
+            self.cart.status = 'active'
+            self.cart.save()
+        elif self.cart.status == 'active' and self.user.last_login:
+            time_since_last_login = timezone.now() - self.user.last_login
+            if time_since_last_login > timedelta(hours=72):
+                self.cart.status = 'abandoned'
+                self.cart.save()
 
     def add_to_cart(self, product_id):
         product = get_object_or_404(Product, id=product_id, is_available=True)
         if product.available_quantity > 0:
             cart_item, created = ShoppingCartItem.objects.get_or_create(cart=self.cart, product=product)
+            self.update_cart_status()
             if product.available_quantity > cart_item.quantity:
                 cart_item.quantity += 1
                 cart_item.save()
@@ -26,12 +36,9 @@ class ShoppingCartMechanism:
                 self.cart.update_total_price()
                 return cart_item
             else:
-                raise ValueError("There are no more of this product avilable")
+                raise ValueError("There are no more of this product available")
         else:
             raise ValueError("Product is out of stock")
-        
-        
-
 
     def remove_from_cart(self, product_id):
         product = get_object_or_404(Product, id=product_id)
@@ -47,13 +54,11 @@ class ShoppingCartMechanism:
         self.cart.update_total_price()
         return cart_item
 
-
-    def clear_cart(self): 
-        cart_items = ShoppingCartItem.objects.filter(cart=self.cart) 
-        for item in cart_items: 
-            item.product.available_quantity += item.quantity 
-            item.product.save() 
-            item.delete() 
-        self.cart.update_total_price() 
+    def clear_cart(self):
+        cart_items = ShoppingCartItem.objects.filter(cart=self.cart)
+        for item in cart_items:
+            item.product.available_quantity += item.quantity
+            item.product.save()
+            item.delete()
+        self.cart.update_total_price()
         return cart_items
-    
